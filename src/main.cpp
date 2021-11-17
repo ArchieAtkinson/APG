508167a666c531573b938fbf915559165d64926d
Archie <46547462+ArchieAtkinson@users.noreply.github.com>
2021-11-17 21:23:55 +0000

    General Tidy up and improvements

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "../external/raylib/src/raylib.h"
 #include "../external/raylib/src/raymath.h"
@@ -20,7 +21,7 @@ const int posterHeight = 4961;
 const int border = int(posterWidth * 0.05);
 const int line_thickness = static_cast<int>(static_cast<float>(border) * 0.05f);
 const int cover_name_gap = border * 1.1f;
-int num_tracks_per_col = 10;
+const int std_num_tracks_per_col = 10;
 
 RenderTexture viewport;
 Texture2D create_album_cover_texture(std::string filepath)
@@ -34,14 +35,54 @@ Texture2D create_album_cover_texture(std::string filepath)
     return texture;
 }
 
+struct Columns{
+    std::string left;
+    std::string right;
+    int count;
+};
+
+Columns create_track_col(std::vector<std::string>& track_list){
+    int left_count, right_count;
+    Columns columns;
+    if(track_list.size() <= std_num_tracks_per_col){
+        left_count = track_list.size();
+        right_count = 0;
+        columns.count = std_num_tracks_per_col;
+    }
+    else if(track_list.size() > std_num_tracks_per_col && track_list.size() < std_num_tracks_per_col * 2){
+        left_count = std_num_tracks_per_col;
+        right_count = track_list.size() - std_num_tracks_per_col;
+        columns.count = std_num_tracks_per_col;        
+    }
+    else {
+        left_count = static_cast<int> (ceil(track_list.size()/2.0f));
+        right_count = static_cast<int> (floor(track_list.size()/2.0f));
+        columns.count = left_count;           
+    }
+
+    for (auto i = 0; i < left_count; i++){
+        columns.left += track_list[i] + "\n ";
+    }
+    for (auto i = left_count; i < right_count + left_count; i++){
+        columns.right += track_list[i] + "\n ";
+    }
+    return columns;
+}
+
+std::string space2underscore(std::string text)
+{
+    std::replace(text.begin(), text.end(), ' ', '_');
+    return text;
+}
+
 int main(int argc, char const *argv[])
 {
     // Initialization
     //--------------------------------------------------------------------------------------
 
     argparse::ArgumentParser program("APG");
-    program.add_argument("artwork").default_value(APG::ROOT_DIR + "/example_data/folklore.png");
-    program.add_argument("info").default_value(APG::ROOT_DIR + "/example_data/folklore.txt");
+    program.add_argument("artwork").default_value(APG::ROOT_DIR + "/example_data/raceforspace.png");
+    program.add_argument("info").default_value(APG::ROOT_DIR + "/example_data/raceforspace.txt");
 
     try
     {
@@ -72,78 +113,73 @@ int main(int argc, char const *argv[])
     auto cover_art_tex = create_album_cover_texture(cover_art_path);
     auto album_info = AlbumInfo(info_file_path);
 
+    TextLine album_name(album_info.album_name, font_black, posterWidth * 0.075f);
+    album_name.set_reference_point({border, static_cast<float>(border) + cover_art_tex.height});
+    album_name.set_offset({0, album_name.size * 0.1f});
+    
+    TextLine artist_name(album_info.artist_name, font_bold, posterWidth * 0.035f);
+    artist_name.set_reference_point(album_name.get_position());
+    artist_name.set_offset({0, album_name.size});
+
+    Vector2 line_start{border, artist_name.get_position().y + artist_name.size * 1.1f};
+    Vector2 line_end{posterWidth - border, line_start.y};
+
+    TextLine album_date(album_info.album_date, font_bold, posterWidth * 0.025f);
+    album_date.set_reference_point({posterWidth - border, line_end.y});
+    album_date.set_offset({-album_date.get_length(), -(artist_name.size * 0.1f + album_date.size)});
+    
+    TextLine album_duration(album_info.album_duration, font_bold, posterWidth * 0.025f);
+    album_duration.set_reference_point(album_date.get_reference_point());
+    album_duration.set_offset({-album_duration.get_length(), artist_name.size * 0.1f});
+    
+    int num_tracks_per_col = std_num_tracks_per_col;
+
+    if(album_info.track_names.size() > std_num_tracks_per_col * 2){
+        num_tracks_per_col = static_cast<int>(ceil(album_info.track_names.size() / 2));
+    }
+
+    Columns columns = create_track_col(album_info.track_names);
+
+    float space_for_track_list = (posterHeight - border) - (line_start.y + artist_name.size * 0.1f);
+    int track_font_size = static_cast<int>(round(space_for_track_list / static_cast<double>(columns.count)));
+
+    float line_length = line_end.x - line_start.x;
+    TextBox left_track_TB(columns.left, font_bold, track_font_size, {line_length, space_for_track_list});
+    left_track_TB.set_reference_point({line_start});
+    left_track_TB.set_offset({0, artist_name.size * 0.1f});
+    
+    TextBox right_track_TB(columns.right, font_bold, track_font_size, {line_length, space_for_track_list});
+    right_track_TB.set_reference_point({line_start.x + (line_start.x + line_end.x)/2, line_start.y});
+    right_track_TB.set_offset({0, artist_name.size * 0.1f});
+        
     while (!WindowShouldClose()) // Detect window close button or ESC ey
     {
         BeginTextureMode(viewport);
 
         DrawRectangle(0, 0, posterWidth, posterHeight, RAYWHITE);
-
         DrawTexture(cover_art_tex, border, border, WHITE);
-
-        TextLine album_name(album_info.album_name, font_black, posterWidth * 0.075f);
-        album_name.set_reference_point({border, static_cast<float>(border) + cover_art_tex.height});
-        album_name.set_offset({0, album_name.size * 0.1f});
-        album_name.draw();
-
-        TextLine artist_name(album_info.artist_name, font_bold, posterWidth * 0.035f);
-        artist_name.set_reference_point(album_name.get_position());
-        artist_name.set_offset({0, album_name.size});
-        artist_name.draw();
-
-        Vector2 line_start{border, artist_name.get_position().y + artist_name.size * 1.1f};
-        Vector2 line_end{posterWidth - border, line_start.y};
         DrawLineEx(line_start, line_end, line_thickness, BLACK);
-
-        TextLine album_date(album_info.album_date, font_bold, posterWidth * 0.025f);
-        album_date.set_reference_point({posterWidth - border, line_end.y});
-        album_date.set_offset({-album_date.get_length(), -(artist_name.size * 0.1f + album_date.size)});
+        album_name.draw();
+        artist_name.draw();
         album_date.draw();
-
-        TextLine album_duration(album_info.album_duration, font_bold, posterWidth * 0.025f);
-        album_duration.set_reference_point(album_date.get_reference_point());
-        album_duration.set_offset({-album_duration.get_length(), artist_name.size * 0.1f});
         album_duration.draw();
-
-        if (album_info.track_names.size() > num_tracks_per_col){
-            num_tracks_per_col = album_info.track_names.size() / 2;
-        }
-        std::vector<std::string> left_tracks_list(album_info.track_names.begin(), album_info.track_names.begin() + num_tracks_per_col);
-        std::vector<std::string> right_tracks_list(album_info.track_names.begin() + num_tracks_per_col, album_info.track_names.end());
-
-        std::string left_tracks;
-        for (auto tracks: left_tracks_list){
-            left_tracks += (tracks + "\n ");
-        }
-
-        std::string right_tracks;
-        for (auto tracks: right_tracks_list){
-            right_tracks += (tracks + "\n ");
-        }
-
-        float space_for_track_list = (posterHeight - border) - (line_start.y + artist_name.size * 0.1f);
-        int track_font_size = static_cast<int>(round(space_for_track_list / static_cast<double>(num_tracks_per_col)));
-
-        float line_length = line_end.x - line_start.x;
-        TextBox left_track_TB(left_tracks, font_bold, track_font_size, {line_length, space_for_track_list});
-        left_track_TB.set_reference_point({line_start});
-        left_track_TB.set_offset({0, artist_name.size * 0.1f});
         left_track_TB.draw();
-
-        TextBox right_track_TB(right_tracks, font_bold, track_font_size, {line_length, space_for_track_list});
-        right_track_TB.set_reference_point({line_start.x + (line_start.x + line_end.x)/2, line_start.y});
-        right_track_TB.set_offset({0, artist_name.size * 0.1f});
         right_track_TB.draw();
 
         EndTextureMode();
 
         BeginDrawing();
+
         ClearBackground(RAYWHITE);
+
         float display_height = static_cast<float>(GetScreenHeight());
         float display_width = static_cast<float>(GetScreenHeight()) / 1.414f;
 
         DrawTexturePro(viewport.texture, {0, 0, posterWidth, -posterHeight},
                        {(GetScreenWidth() - display_width) / 2, 0, display_width, display_height}, {0, 0}, 0.0f, WHITE);
+
         int line_thickness = GetScreenHeight() * 0.002f > 1 ? static_cast<int> (round( GetScreenHeight() * 0.002f)) : 1;
+
         DrawRectangleLinesEx({(GetScreenWidth() - display_width) / 2, 0, display_width, display_height}, line_thickness, BLACK);
 
         EndDrawing();
@@ -154,8 +190,9 @@ int main(int argc, char const *argv[])
 #ifndef DEBUG
     Image viewport_img = LoadImageFromTexture(viewport.texture);
     ImageFlipVertical(&viewport_img);
+    
     const char *output_file_name =
-        TextFormat("%s - %s.png", album_info.album_name.c_str(), album_info.artist_name.c_str());
+        TextFormat("%s-%s.png", space2underscore(album_info.album_name).c_str(), space2underscore(album_info.artist_name).c_str());
     ExportImage(viewport_img, output_file_name);
     UnloadImage(viewport_img);
 #endif
